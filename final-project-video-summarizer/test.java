e180cf39e0a4b22cc31edc08b1cc1567e8a8eabf
tianyia <55004002+tianyia@users.noreply.github.com>
2021-05-25 12:34:44 -0700

    Create test.java

---
+++ final-project-video-summarizer/test.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class test {
+	public static int width = 320;
+	public static int height = 180;
+	
+
+	// public static void main(String[] args) {
+	// 	//Path folderPath = Paths.get("");
+	// 	File folder = new File("../project_dataset/frames_rgb/concert");
+	//     List<File> fileList = Arrays.asList(folder.listFiles());
+		
+	//     //get sorted list of frames from folder path
+	// 	FrameOperations op = new FrameOperations();
+	// 	MotionVector mv = new MotionVector();
+	// 	List<File> list = op.getFrameList(fileList);
+		
+	// 	//find the list of key frames for shots
+	// 	ArrayList<Integer> keyFrames = mv.findKeyFrames(list);
+	// 	System.out.println("number of key frames is: " + keyFrames.size());
+	// 	for(int i = 0; i < keyFrames.size(); i++) {
+	// 		System.out.print(keyFrames.get(i) + ", ");
+	// 	}
+		
+	// }
+
+}
